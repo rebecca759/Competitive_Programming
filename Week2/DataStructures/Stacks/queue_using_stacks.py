65bbb515ba7fdfb7d37b8f20ce448fffd8ecd6d3
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-01-11 23:17:31 +0300

    Updated file

--- Week2/DataStructures/Stacks/queue_using_stacks.py
+++ Week2/DataStructures/Stacks/queue_using_stacks.py
@@ -1,43 +1,47 @@
 class MyQueue:
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
         self.stack1 = []
         self.stack2 = []
+        self.front = 0
         
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
-        while len(self.stack1) > 0:
-            self.stack2.append(self.stack1.pop())
+        if len(self.stack1) == 0:
+            self.front = x
             
         self.stack1.append(x)
         
-        while len(self.stack2) > 0:
-            self.stack1.append(self.stack2.pop())
-        
-
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
-        return self.stack1.pop()
+        if len(self.stack2) == 0:
+            while (len(self.stack1) > 0):
+                self.stack2.append(self.stack1.pop())
+                
+        return self.stack2.pop()
 
     def peek(self) -> int:
         """
         Get the front element.
         """
-        return self.stack1[-1]
+        if not (self.stack2 == []):
+            return self.stack2[-1]
         
+        return self.front
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-        return self.stack1 == []
+        return (self.stack1 == []) and (self.stack2 == [])
 
 q = MyQueue()
 q.push(1)
