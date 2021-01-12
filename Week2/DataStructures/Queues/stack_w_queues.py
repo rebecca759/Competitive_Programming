670c19ba769ecb61324e7bb7a4500f424f3e2687
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-01-12 16:04:24 +0300

    updated answer

--- Week2/DataStructures/Queues/stack_w_queues.py
+++ Week2/DataStructures/Queues/stack_w_queues.py
@@ -1,25 +1,35 @@
+from collections import deque
 class MyStack:
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.queue1 = []
-        self.queue2 = []
+        self.queue1 = deque()
+        self.queue2 = deque()
+        self.top_elem = 0
 
     def push(self, x: int) -> None:
         """
         Push element x onto stack.
         """
-        self.queue1.insert(0,x)
-
+        self.queue1.appendleft(x)
+        self.top_elem = x
+        
+        
     def pop(self) -> int:
         """
         Removes the element on top of the stack and returns that element.
         """
         
-        while len(self.queue1) > 0:
-            self.queue2.append(self.queue1.pop())
+        while len(self.queue1) > 1:
+            self.top_elem = self.queue1.pop()
+            self.queue2.appendleft(self.top_elem)
             
+        temp = self.queue1
+        self.queue1 = self.queue2
+        self.queue2 = temp
+        
+        
         return self.queue2.pop()
         
         
@@ -27,18 +37,14 @@ class MyStack:
         """
         Get the top element.
         """
-        while len(self.queue1) > 0:
-            self.queue2.append(self.queue1.pop())
-            
-        return self.queue2[-1]
+        return self.top_elem
         
         
     def empty(self) -> bool:
         """
         Returns whether the stack is empty.
         """
-        return len(self.queue1) == 0 and len(self.queue2) == 0
-        
+        return len(self.queue1) == 0        
         
 
 stack = MyStack()
