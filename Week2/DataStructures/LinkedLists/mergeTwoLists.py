831d4a1542d16fcbf98fbe11f387b43ca29ca954
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-01-16 04:34:20 +0300

    updated code

--- Week2/DataStructures/LinkedLists/mergeTwoLists.py
+++ Week2/DataStructures/LinkedLists/mergeTwoLists.py
@@ -1,38 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         result = ListNode()
         head = result
         
-        while l1 or l2:
-            a = l1.val if l1 else -999
-            b = l2.val if l2 else -999
-            
-            if a < b and a != -999:
-                result.next = ListNode(a)
-                l1 = l1.next
-                result = result.next
-                    
-            elif a < b and a == -999:
-                result.next = ListNode(b)
-                l2 = l2.next
-                result = result.next
-                
-            elif a > b and b != -999:
-                result.next = ListNode(b)
-                l2 = l2.next
-                result = result.next
-                    
-            elif a > b and b == -999:
-                result.next = ListNode(a)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                result.next = l1
                 l1 = l1.next
                 result = result.next
                 
-            elif a == b:
-                result.next = ListNode(a)
-                l1 = l1.next
-                result = result.next
-                result.next = ListNode(b)
+            elif l1.val > l2.val:
+                result.next = l2
                 l2 = l2.next
                 result = result.next
+                    
+        result.next = l1 if l1 else l2
                 
         return head.next
