2b255407621341ce023f5fa146a269765e8cde0b
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-01-18 12:40:28 +0300

    added files

---
+++ Week2/goodPairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        num_count = {}
+        pairs = 0
+        
+        for num in nums:
+            if num in num_count:
+                num_count[num] += 1
+                
+            else:
+                num_count[num] = 1
+                
+        for num in num_count:
+            if num_count[num] > 1:
+                pair = num_count[num]-1
+                pairs += pair
+                
+                while pair > 1:
+                    pair = pair-1
+                    pairs += pair
+                    
+        return pairs
+            
+        
+
+        
