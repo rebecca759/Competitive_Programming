0cef896f7f0148524ec5b6f71b76258348b95466
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-01-23 21:49:24 +0300

    added file

---
+++ Week4/Trees/searchInBST.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return
+        
+        if val < root.val:
+            return self.searchBST(root.left, val)
+            
+        elif val > root.val:
+            return self.searchBST(root.right, val)
+            
+        elif root.val == val:
+            return root
+        
