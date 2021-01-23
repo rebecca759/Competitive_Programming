baa394aeafedc8d7a364bbd28e9eca5831c36e22
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-01-23 21:08:16 +0300

    added files

--- Week3/AdvancedSorting/mergeSort.py
+++ Week3/AdvancedSorting/mergeSort.py
@@ -1,43 +1,47 @@
-#Merge Sort
-
-def mergeSort(arr,start,end,r):
-    if start < end:
-        mid = (start + end) // 2
-        p1 = mergeSort(arr,start,mid,r)
-        p2 = mergeSort(arr,mid + 1,end,r)
-
-        #merge the two halves
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:        
+        return self.mergeSort(nums,0,len(nums)-1,[])
         
-        r = mergeLists(p1,r)
-        r = mergeLists(p2,r)
-
-        return r
         
-    return [arr[start]]
+    #Merge Sort
 
-def mergeLists(l1,l2):
-    i = j = 0
+    def mergeSort(self,arr,start,end,r):
+        if start < end:
+            mid = (start + end) // 2
+            p1 = self.mergeSort(arr,start,mid,r)
+            p2 = self.mergeSort(arr,mid + 1,end,r)
 
-    result = []
+            #merge the two halves
 
-    while i < len(l1) or j < len(l2):
-        if i == len(l1):
-            result += l2[j:]
-            break
+            r = self.mergeLists(p1,r)
+            r = self.mergeLists(p2,r)
 
-        if j == len(l2):
-            result += l1[i:]
-            break
-    
-        if l1[i] <= l2[j]:
-            result.append(l1[i])
-            i += 1
+            return r
 
-        elif l2[j] < l1[i]:
-            result.append(l2[j])
-            j += 1
+        return [arr[start]]
 
-    return result
+    def mergeLists(self,l1,l2):
+        i = j = 0
 
-arr = [38,27,43,3,9,82,10]
-print(mergeSort(arr,0,len(arr)-1,[]))
+        result = []
+
+        while i < len(l1) or j < len(l2):
+            if i == len(l1):
+                result += l2[j:]
+                break
+
+            if j == len(l2):
+                result += l1[i:]
+                break
+
+            if l1[i] <= l2[j]:
+                result.append(l1[i])
+                i += 1
+
+            elif l2[j] < l1[i]:
+                result.append(l2[j])
+                j += 1
+
+        return result
+        
+
--- Week3/AdvancedSorting/quickSort.py
+++ Week3/AdvancedSorting/quickSort.py
@@ -1,33 +1,34 @@
-def quickSort(arr, start, end):
-    if start >= end:
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:        
+        return self.quickSort(nums,0,len(nums)-1)
+        
+        
+    def quickSort(self,arr, start, end):
+
+        if start >= end:
+            return arr
+
+        elif (start < end):
+            p = self.partition(arr,start,end)
+            self.quickSort(arr,start,p - 1)
+            self.quickSort(arr, p + 1,end)
+
         return arr
-    
-    elif (start < end):
-        p = partition(arr,start,end)
-        quickSort(arr,start,p - 1)
-        quickSort(arr, p + 1,end)
-
-    return arr
         
 
-def partition(arr,start,end):
-    pivot_index = start
-    store_index = pivot_index + 1
-    swap = False
-    
-    for i in range(pivot_index + 1,end+1):
-        if arr[i] < arr[pivot_index]:
-            arr[i], arr[store_index] = arr[store_index], arr[i]
-            store_index += 1
-            swap = True
+    def partition(self,arr,start,end):
+        pivot_index = start
+        store_index = pivot_index + 1
+        swap = False
 
-    if swap:
-        arr[pivot_index] , arr[store_index - 1] = arr[store_index - 1],arr[pivot_index]
-        pivot_index = store_index - 1
+        for i in range(pivot_index + 1,end+1):
+            if arr[i] < arr[pivot_index]:
+                arr[i], arr[store_index] = arr[store_index], arr[i]
+                store_index += 1
+                swap = True
 
-    return pivot_index
-        
+        if swap:
+            arr[pivot_index] , arr[store_index - 1] = arr[store_index - 1],arr[pivot_index]
+            pivot_index = store_index - 1
 
-arr = [3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
-
-print(quickSort(arr,0,len(arr)-1))
+        return pivot_index
